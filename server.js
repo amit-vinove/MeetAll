df9ef8d56344757eee012e46cb422920f67e1b03
Amit Kumar <amit.kumar3@mail.vinove.com>
2022-03-10 10:39:18 +0530

    first commit

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-if (process.env.NODE_ENV !== "production") require("dotenv").config();
+// if (process.env.NODE_ENV !== "production") require("dotenv").config();
+require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
